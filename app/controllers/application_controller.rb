3a3c602634d6ffce8508bd4126752f69909f2c14
jcmcarth06 <jcmcarth06@gmail.com>
2020-06-16 15:40:06 -0600

    update routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,32 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  get '/ice_creams' do
+    "all"
+  end
+
+  get '/ice_creams/new' do
+    "provide a form"
+  end
+
+  post '/ice_creams' do
+    "take in form input and create through controller"
+  end
+
+  get '/ice_creams/:id' do
+    "show"
+  end
+
+  get '/ice_creams/:id/edit' do
+    "return an HTML form for editing an object (ice_cream)"
+  end
+
+  put '/ice_creams/:id' do
+    "update ice_cream based on changes in /edit"
+  end
+
+  delete '/ice_creams/:id' do
+    "destroy ice_cream"
+  end
+
 end
